6e89c5da61a67bffbf0c5efd78192bf40afd23e4
silvinaas <silvinaas4@gmail.com>
2021-08-28 00:51:45 +0000

    pronto

--- src/app.js
+++ src/app.js
@@ -7,5 +7,24 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  function dominio() {
+    let domain = [];
+    for (let i = 0; i < pronoun.length; i++) {
+      for (let e = 0; e < adj.length; e++) {
+        for (let a = 0; a < noun.length; a++) {
+          let pron = pronoun[i];
+          let adjetive = adj[e];
+          let sust = noun[a];
+
+          domain.push(pron + adjetive + sust);
+        }
+      }
+    }
+    return domain;
+  }
+  console.log(dominio());
 };
